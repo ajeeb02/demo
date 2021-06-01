7fba09eedbad4e50ab5fc336855322d7c4f2cbbd
ajeeb02 <23136@harsh@g,mail.com>
2021-06-01 22:34:38 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int e;
+
+        int j;
+        int img;
+        char z = 0;
+        Scanner k = new Scanner(System.in);
+
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i]);
+        }
+        }
+
+
+
+}
+
+
+
+
+
+
+
